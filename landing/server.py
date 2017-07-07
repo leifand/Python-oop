5716b75ef15d6df8b284385fe21ef1dfc7af2443
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-07 13:48:07 -0500

    continuing Landing Page

---
+++ landing/server.py
@@ -0,0 +1,23 @@
+'''
+    server.py
+    Landing Page Exercise
+    Leif Anderson 7/7/17
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template('ninjas.html')
+
+@app.route('/dojos/new')
+def new_ninja():
+    pass
+
+# Run, server, run!
+app.run(Debug=True)
---
+++ landing/static/js/script.js
@@ -0,0 +1,5 @@
+/*
+    script.js
+    Landing Page Exercise
+    Leif Anderson 7/7/17
+*/
